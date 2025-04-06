2c627d702e76ef53439b4f1304f5d5f0e4361f1a
Jinny You <baram991103@gmail.com>
2025-04-06 23:22:37 +0900

    Update logo

--- lib/src/core/langtool_images.dart
+++ lib/src/core/langtool_images.dart
@@ -1,7 +1,7 @@
 /// Class [LangToolImages] for managing the images used in LanguageTool.
 class LangToolImages {
   /// LanguageTool logo
-  static const logo = 'assets/icon/splash_icon.png';
+  static const logo = 'assets/icon/splash_icon_new.png';
 
   LangToolImages._();
 }
